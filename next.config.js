b006d96fdd52e07c0aaaf60afc065b3f2cb31195
Maximiliano Ayala <max@youthwar.com>
2021-09-12 21:06:42 -0700

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withMDX = require('@next/mdx')();
+
+
+module.exports = withMDX({
+  pageExtensions: ['tsx','ts', 'mdx']
+})
